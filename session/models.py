46354efd3b12b7ff9654d2ac9d214f0b7ea9d588
Mohamad Nashaat <mohamad.nashaat99@gmail.com>
2022-03-19 21:40:17 +0300

    Update the related field name on session/session_student model

--- session/models.py
+++ session/models.py
@@ -7,22 +7,28 @@ from person.models import Person
 
 # Create your models here.
 
+
 class Session(models.Model):
     session_id = models.IntegerField(primary_key=True)
     session_number = models.IntegerField(null=True)
     course_id = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
     level_id = models.ForeignKey(Level, on_delete=models.CASCADE, null=True)
-    position_id = models.ForeignKey(Position, on_delete=models.CASCADE, null=True)
+    position_id = models.ForeignKey(
+        Position, on_delete=models.CASCADE, null=True)
     time_id = models.ForeignKey(Time, on_delete=models.CASCADE, null=True)
     teacher_id = models.ForeignKey(Person, null=True, on_delete=models.CASCADE)
     create_date = models.DateField(auto_now_add=True, null=True)
+
     def __str__(self):
         return f'{self.session_id}'
 
+
 class Session_Student(models.Model):
     id = models.IntegerField(primary_key=True)
-    session_id = models.ForeignKey(Session, on_delete=models.CASCADE, null=True)
+    session_id = models.ForeignKey(
+        Session, on_delete=models.CASCADE, null=True, related_name='sessions')
     student_id = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
     create_date = models.DateField(auto_now_add=True, null=True)
+
     def __str__(self):
         return f'{self.id}'
