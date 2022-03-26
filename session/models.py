6006923cd9ab33f498b6d6d32a264e46b7ee63a5
Mohamad Nashaat <mohamad.nashaat99@gmail.com>
2022-03-26 14:37:44 +0300

    Update related_name into session model

--- session/models.py
+++ session/models.py
@@ -16,7 +16,8 @@ class Session(models.Model):
     position_id = models.ForeignKey(
         Position, on_delete=models.CASCADE, null=True)
     time_id = models.ForeignKey(Time, on_delete=models.CASCADE, null=True)
-    teacher_id = models.ForeignKey(Person, null=True, on_delete=models.CASCADE)
+    teacher_id = models.ForeignKey(
+        Person, null=True, on_delete=models.CASCADE, related_name='teachers')
     create_date = models.DateField(auto_now_add=True, null=True)
 
     def __str__(self):
@@ -27,7 +28,8 @@ class Session_Student(models.Model):
     id = models.IntegerField(primary_key=True)
     session_id = models.ForeignKey(
         Session, on_delete=models.CASCADE, null=True, related_name='sessions')
-    student_id = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
+    student_id = models.ForeignKey(
+        Person, on_delete=models.CASCADE, null=True, related_name='students')
     create_date = models.DateField(auto_now_add=True, null=True)
 
     def __str__(self):
